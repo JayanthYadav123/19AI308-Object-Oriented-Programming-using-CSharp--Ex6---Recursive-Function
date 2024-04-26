fedbc9f0dc855bd118b20ae3c6e5456964a8ead3
JayanthYadav123 <94836154+JayanthYadav123@users.noreply.github.com>
2024-04-26 21:51:24 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,32 +1,26 @@
 using System;
-namespace program11{
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Program
+{
     class Program
     {
-        static int ReverseNumber(int num, int reversedNum = 0)
-        {
-            // Base case: if the number becomes 0, return the reversed number
-            if (num == 0)
-                return reversedNum;
-            
-            // Extract the last digit of the number
-            int lastDigit = num % 10;
-            
-            // Add the last digit to the reversed number
-            reversedNum = reversedNum * 10 + lastDigit;
-            
-            // Recursively call ReverseNumber with the remaining digits
-            return ReverseNumber(num / 10, reversedNum);
-        }
-    
         static void Main(string[] args)
         {
-            // Input the number to be reversed
-            Console.Write("Enter a number to reverse: ");
-            int number = Convert.ToInt32(Console.ReadLine());
-    
-            // Call the ReverseNumber function and print the reversed number
-            int reversedNumber = ReverseNumber(number);
-            Console.WriteLine("Reversed Number: " + reversedNumber);
+            int num, reverse = 0;
+            Console.Write("Enter a Number : ");
+            num = int.Parse(Console.ReadLine());
+            while (num != 0)
+            {
+                reverse = reverse * 10;
+                reverse = reverse + num % 10;
+                num = num / 10;
+            }
+            Console.WriteLine("Reverse of Entered Number is : " + reverse);
+            Console.ReadLine();
+
         }
     }
 }
